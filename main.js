8be165116c13c2b05eb05c2ef7684896bf35308e
John Schimenz <john.schimenz@gmail.com>
2023-08-08 12:02:16 -0500

    Creat buttons and operation functions

--- main.js
+++ main.js
@@ -1,24 +1,56 @@
-function add (a, b)
+// Variable declarations
+let numberA;
+let operator = "";
+let numberB;
+
+function operate (numberA, numberB, operator)
 {
-    return a + b;
+    if (operator === "+")
+    {
+        return add(numberA, numberB);
+    }
+    if (operator === "-")
+    {
+        return subtract(numberA, numberB);
+    }
+    if (operator === "*")
+    {
+        return multiply(numberA, numberB);
+    }
+    if (operator === "/")
+    {
+        return divide(numberA, numberB);
+    }
 }
 
-function substract (a, b)
+// Operator functions
+function add (numberA, numberB)
 {
-    return a - b;
+    return numberA + numberB;
 }
 
-function multiply (a, b)
+function subtract (numberA, numberB)
 {
-    return a * b;
+    return numberA - numberB;
 }
 
-function divide (a, b)
+function multiply (numberA, numberB)
 {
-    return a / b;
+    return numberA * numberB;
+}
+
+function divide (numberA, numberB)
+{
+    return numberA / numberB;
 }
 
 console.log(add(5, 2));
 console.log(subtract(5, 2));
 console.log(multiply(5, 2));
 console.log(divide(5, 2));
+
+console.log(operate(5, 2, "+"))
+// Works for addition but not other operations
+console.log(operate(5, 2, "-"));
+console.log(operate(5, 2, "*"));
+console.log(operate(5, 2, "/"));
