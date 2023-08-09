edfab3dcfb86e18e65ba2fc2bb3e7a1b0ce6bfaf
John Schimenz <john.schimenz@gmail.com>
2023-08-09 13:07:19 -0500

    reset operator functions

--- main.js
+++ main.js
@@ -107,20 +107,20 @@ function operate(numberA, numberB, operator)
 // Operator functions
 function add (numberA, numberB)
 {
-    return display = numberA + numberB;
+    return numberA + numberB;
 }
 
 function subtract (numberA, numberB)
 {
-    return display = numberA - numberB;
+    return numberA - numberB;
 }
 
 function multiply (numberA, numberB)
 {
-    return display = numberA * numberB;
+    return numberA * numberB;
 }
 
 function divide (numberA, numberB)
 {
-    return display = numberA / numberB;
+    return numberA / numberB;
 }
