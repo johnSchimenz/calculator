5e427dd37d31aa1a3da21ab1d74f581a29774ff7
John Schimenz <john.schimenz@gmail.com>
2023-08-09 13:05:54 -0500

    Complete digits, decimal, clear, operate, and operator functions

--- main.js
+++ main.js
@@ -2,8 +2,89 @@
 let numberA;
 let operator = "";
 let numberB;
+let display = 0;
+let operatorCounter = 0;
+let arrayInputs = [numberA, numberB];
+const buttonDisplay = document.querySelector("#display");
+const buttonsDigits = document.querySelectorAll("button.digit");
+const buttonDecimal = document.querySelector("#decimal");
+const buttonClear = document.querySelector("#clear");
+const buttonEquals = document.querySelector("#equals"); // Need to finish this "click" steps
+const buttonsOperators = document.querySelectorAll("button.operator");
 
-function operate (numberA, numberB, operator)
+// What happens on button click - IN PROGRESS
+buttonsDigits.forEach((button) =>
+{
+    button.addEventListener("click", () =>
+    {
+        if (display === 0)
+        {
+            display = button.textContent;
+            console.log(display);
+            return display;
+        }
+        else
+        {
+            display = display + button.textContent;
+            console.log(display);
+            return (display);            
+        }
+
+    })
+})
+
+//DONE
+buttonDecimal.addEventListener("click", () =>
+{
+    if (display.includes(".") === false)
+    {
+        display = display + ".";
+        console.log(display);
+    }
+})
+
+// IN PROGRESS
+buttonsOperators.forEach((button) =>
+{
+    button.addEventListener("click", () =>
+    {
+        operatorCounter++;
+        if (operatorCounter === 1)
+        {
+            numberB = display;
+        }
+        
+        else if (operatorCounter > 1)
+        {
+            numberA = numberB;
+            numberB = display;
+            operator = button.textContent;
+            operate(numberA, numberB, operator);
+
+        }
+    })
+})
+
+
+//DONE
+buttonClear.addEventListener("click", () => 
+{
+    display = "";
+    console.log(display);
+})
+
+// Need to finish the "equals" steps below
+/*
+buttonEquals.addEventListener("click", () =>
+{
+    numberA = 
+    numberB = display;
+
+})
+*/
+
+// Perform operation
+function operate(numberA, numberB, operator)
 {
     if (operator === "+")
     {
@@ -26,31 +107,20 @@ function operate (numberA, numberB, operator)
 // Operator functions
 function add (numberA, numberB)
 {
-    return numberA + numberB;
+    return display = numberA + numberB;
 }
 
 function subtract (numberA, numberB)
 {
-    return numberA - numberB;
+    return display = numberA - numberB;
 }
 
 function multiply (numberA, numberB)
 {
-    return numberA * numberB;
+    return display = numberA * numberB;
 }
 
 function divide (numberA, numberB)
 {
-    return numberA / numberB;
+    return display = numberA / numberB;
 }
-
-console.log(add(5, 2));
-console.log(subtract(5, 2));
-console.log(multiply(5, 2));
-console.log(divide(5, 2));
-
-console.log(operate(5, 2, "+"))
-// Works for addition but not other operations
-console.log(operate(5, 2, "-"));
-console.log(operate(5, 2, "*"));
-console.log(operate(5, 2, "/"));
